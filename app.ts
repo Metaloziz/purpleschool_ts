2d8dbe3f9c7302f689a405284a3be3c76530bef6
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-11-26 12:00:00 +0300

    lesson_15 Void

--- app.ts
+++ app.ts
@@ -6,3 +6,19 @@ type User = {
 let a: User = {
   id: 10
 }
+
+
+const foo = () => {
+  console.log('')
+}
+
+let b = foo()
+
+
+const foo2 = () => {
+  return typeof b
+}
+
+let c = foo2()
+
+console.log(c)

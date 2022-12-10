b53dc26cec98e1b65e69f1f0a3c762ac73e1e3e4
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-12-10 12:00:00 +0300

    lesson_20

--- app.ts
+++ app.ts
@@ -1,4 +1,44 @@
-let a = 'qwe'
-let b: number = +a
+const App = (x: string | number): x is string => {
 
-console.log(b)
+  return typeof x === 'string'
+
+}
+
+
+console.log(App('1'))
+
+
+// более наглядно на сложном типе
+
+type User = {
+  id: string
+}
+
+type Admin = {
+  id: string
+  role: 'admin'
+}
+
+function isAdmin(arg: User | Admin): arg is Admin {
+
+  // return 'role' in arg
+  // or
+  return (arg as Admin).role !== undefined
+
+}
+
+// эта функция сужает тип при использовании её в if...else
+
+
+function foo (arg: User | Admin){
+
+  if(isAdmin(arg)){
+    arg
+   // внутри arg есть ДВА ключа
+  } else {
+    arg
+    // внутри arg есть ОДИН ключ
+  }
+
+
+}

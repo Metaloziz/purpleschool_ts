c9237abe9142e1a628060164818d04234ad90812
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-12-17 12:00:00 +0300

    lesson_22 constructor

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 class User {
   name: string
 
+  constructor(name?: string) {
+
+    this.name = name ?? 'no name'
+
+  }
+
 }
 
 const user = new User()
 console.log(user)
-user.name = 'AAAAA'
-console.log(user);
+

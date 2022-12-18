8ef71e3b3a66c074f3875f9c6a8b354d53840fc7
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-12-18 12:00:00 +0300

    lesson_23 methods of constructor

--- app.ts
+++ app.ts
@@ -1,14 +1,48 @@
-class User {
-  name: string
+enum PaymentStatus {
+  Holding,
+  Process,
+  Reversed
+}
 
-  constructor(name?: string) {
 
-    this.name = name ?? 'no name'
+class Payment {
+  id: number
+  status = PaymentStatus.Holding
+  creatAt = new Date()
+  updateAt: Date
+
+  constructor(id: number) {
+    this.id = id
 
   }
 
+  getPaymentLifeTime(): number {
+    return new Date().getTime() - this.creatAt.getTime()
+  }
+
+  unholdPayment(): void {
+    if (this.status === PaymentStatus.Process) {
+      throw  new Error('Платёж не может быть возвращён!')
+    }
+    this.status = PaymentStatus.Reversed
+    this.updateAt = new Date()
+
+  }
 }
 
-const user = new User()
-console.log(user)
+
+const payment = new Payment(1)
+
+payment.unholdPayment()
+console.log(payment)
+
+const time = payment.getPaymentLifeTime()
+
+
+console.log(time);
+
+
+
+
+
 

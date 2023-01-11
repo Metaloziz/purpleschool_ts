2b9019c9c14cfb6e7181e086cc18f02436fab792
Andrew Gaityukevich <AndrewGaity@yandex.by>
2023-01-11 12:00:00 +0300

    lesson_39 Template Literal Types

--- app.ts
+++ app.ts
@@ -1,37 +1,9 @@
-// Indexed Access Types
 
 
 type Roles = 'admin' | 'student' | 'parent'
 
 
-type MapperType<T> = {
-  [key in keyof T]: Roles
-}
-
-type UserType2 = {
-  user1: any
-  user2: any
-  user3: any
-
-}
-
-type ConvertAllKeysType = MapperType<UserType2>
+type UserType = `user: ${Capitalize<Roles>}`
 
 
-let AA22: ConvertAllKeysType = {
-  user1: 'admin',
-  user2: 1,   // все типы ANY заменились
-  user3: null
-}
-
-
-type UsersType = {
-
-  [key: string]: Roles
-
-}
-
-
-let a: UsersType = {
-  'asdsd': "admin"
-}
+let user1: UserType = "user: Admin"
